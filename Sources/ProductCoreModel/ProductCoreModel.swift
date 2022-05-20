a9ab5fc3d075fc4872e47974f31997d2d277a1ba
Nikunj <nikunjmodi86@gmail.com>
2022-05-20 15:57:19 +0530

    Change protection level.

--- Sources/ProductCoreModel/ProductCoreModel.swift
+++ Sources/ProductCoreModel/ProductCoreModel.swift
@@ -6,14 +6,14 @@ public struct Products: Codable {
 
 // MARK: - Product
 public struct Product: Codable {
-    let id: Int
-    let title, productDescription: String
-    let price: Int
-    let discountPercentage, rating: Double
-    let stock: Int
-    let brand, category: String
-    let thumbnail: String
-    let images: [String]
+    public let id: Int
+    public let title, productDescription: String
+    public let price: Int
+    public let discountPercentage, rating: Double
+    public let stock: Int
+    public let brand, category: String
+    public let thumbnail: String
+    public let images: [String]
 
     enum CodingKeys: String, CodingKey {
         case id, title
